5bc56f6f9875ea3253f4b03df909d1e92c0091ea
Zachary Guard <zacharyguard@gmail.com>
2017-08-17 16:37:14 +0800

    Switch from json to heroku config for firebase service account key

--- server.js
+++ server.js
@@ -15,7 +15,18 @@ firebase.initializeApp({
 })
 
 const fAdmin = require("firebase-admin")
-const fServiceAccountKey = require(path.join(__dirname, "service_account_key.json"));
+const fServiceAccountKey = {
+  type: process.env.SAK_TYPE,
+  project_id: process.env.SAK_PROJECT_ID,
+  private_key_id: process.env.SAK_PRIVATE_KEY_ID,
+  private_key: process.env.SAK_PRIVATE_KEY,
+  client_email: process.env.SAK_CLIENT_EMAIL,
+  client_id: process.env.SAK_CLIENT_ID,
+  auth_uri: process.env.SAK_AUTH_URI,
+  token_uri: process.env.SAK_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.SAK_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.SAK_CLIENT_X509_CERT_URL
+}
 fAdmin.initializeApp({
   credential: fAdmin.credential.cert(fServiceAccountKey),
   databaseURL: "https://eventhub-7cde3.firebaseio.com"
